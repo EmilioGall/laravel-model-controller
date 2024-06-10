97c97bc2cf5afb156915ee1f8e49e700c81c1770
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-10 21:07:30 +0200

    feat: print cards of movies on page - home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-   <main>
+   <main class="container-fluid my-3">
 
-      <h1 class="text-center">
+      <h1 class="fs-1 fw-bold text-center mb-3"> Movies List </h1>
 
-         Laravel-Vite Template <br>
+      <div class="row g-3 row-cols-1 row-cols-sm-2 row-cols-lg-5">
 
-         <em class="fs-4">
-            with Bootstrap <i class="fa-brands fa-bootstrap"></i> and Fontawesome <i class="fa-solid fa-font-awesome"></i>
-         </em>
+         @foreach ($moviesArray as $movie)
+            <div class="col d-flex align-items-stretch">
 
-      </h1>
+               <div class="card">
+
+                  <img src="{{ $movie['image'] }}" class="card-img-top" alt="Poster of {{ $movie['title'] }}">
+
+                  <div class="card-body">
+
+                     <h3 class="card-title m-0">{{ $movie['title'] }}</h3>
+
+                     <h5 class="card-subtitle mb-2"><em>{{ $movie['original_title'] }}</em></h5>
+
+                     <p class="card-text m-0">{{ $movie['date'] }}</p>
+
+                     <p class="card-text m-0">Nationality: {{ $movie['nationality'] }}.</p>
+
+                     <p class="card-text m-0">{{ $movie['vote'] }} /10</p>
+                  </div>
+
+               </div>
+
+            </div>
+         @endforeach
+
+      </div>
 
    </main>
 @endsection
